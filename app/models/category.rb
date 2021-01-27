ae151c4df23225e96bbcba910cfa4e8cf57c77b3
eRy-sk <ride.poke@gmail.com>
2021-01-27 18:41:28 +0900

    いったん削除

--- app/models/category.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Category < ActiveYaml::Base
-  set_root_path Rails.root.join('app/models/active_yaml/data')
-  set_filename 'category'
-
-  include ActiveHash::Associations
-  has_many :payments, dependent: :destroy
-end
